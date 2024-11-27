0010a6146f560d7eeeedca07d267e646e99ae514
alvdelci <delcimarioalvesdev@gmail.com>
2024-11-27 14:59:50 -0300

    feat: Definindo metodo http connectCar

---
+++ src/main/java/io/github/alvdelci/springarchitecture/carmaker/api/FactoryTestController.java
@@ -0,0 +1,23 @@
+package io.github.alvdelci.springarchitecture.carmaker.api;
+
+import io.github.alvdelci.springarchitecture.carmaker.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("cars")
+public class FactoryTestController {
+
+    @Autowired //Isso é uma injecao de dependencia. Injetando o bean Engine, do configuration, para nao precisar instanciar.
+    private Engine engine;
+
+    @PostMapping
+    public StatusCar connectCar(@RequestBody Key key) {
+        Car car = new HondaHRV(engine);
+
+        return car.ignite(key);
+    }
+}
