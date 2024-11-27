377584da2373292cc2f58a862e5aa0291431b5b6
alvdelci <delcimarioalvesdev@gmail.com>
2024-11-27 15:18:08 -0300

    chore: Corrigindo mensagem de retorno

--- src/main/java/io/github/alvdelci/springarchitecture/carmaker/Car.java
+++ src/main/java/io/github/alvdelci/springarchitecture/carmaker/Car.java
@@ -47,7 +47,7 @@ public class Car {
 
     public StatusCar ignite(Key key) {
         if(key.getCarmaker() != this.carmaker) {
-            return new StatusCar("This does not belong to this car.");
+            return new StatusCar("This key does not belong to this car.");
         }
         return new StatusCar("Car connected. Running with the engine " + engine);
     }
