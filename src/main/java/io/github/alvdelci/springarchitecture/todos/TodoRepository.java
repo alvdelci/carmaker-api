50773867329c28a122f6c0347e77ec515927fd3f
alvdelci <delcimarioalvesdev@gmail.com>
2024-11-27 17:33:38 -0300

    feat: Trabalhando com Components

--- src/main/java/io/github/alvdelci/springarchitecture/todos/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.alvdelci.springarchitecture.todos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepository extends JpaRepository<TodoEntity, Integer> {
-    boolean existsByDescription(String description);
-}
