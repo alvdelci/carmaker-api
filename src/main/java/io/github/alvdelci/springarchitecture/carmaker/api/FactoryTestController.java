66be7f831d5a2420d0e5f91bfff54e15f4e69d4d
alvdelci <delcimarioalvesdev@gmail.com>
2024-11-27 15:19:24 -0300

    refactor: Utilizando Qualifier para passar um bean especifico para injecao

--- src/main/java/io/github/alvdelci/springarchitecture/carmaker/api/FactoryTestController.java
+++ src/main/java/io/github/alvdelci/springarchitecture/carmaker/api/FactoryTestController.java
@@ -2,6 +2,7 @@ package io.github.alvdelci.springarchitecture.carmaker.api;
 
 import io.github.alvdelci.springarchitecture.carmaker.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("cars")
 public class FactoryTestController {
 
-    @Autowired //Isso é uma injecao de dependencia. Injetando o bean Engine, do configuration, para nao precisar instanciar.
+    @Autowired
+    @Qualifier("turboEngine")
+//Isso é uma injecao de dependencia. Injetando o bean Engine, do configuration, para nao precisar instanciar.
     private Engine engine;
 
     @PostMapping
